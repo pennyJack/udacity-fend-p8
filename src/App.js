2a5087a4515012e46f6902ddda94829a02059d17
Andre Wibbeke <andre.wibbeke@gmail.com>
2018-09-04 07:21:44 +0200

    feature: get places from Foursquare API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react'
+import * as LocationAPI from './LocationAPI'
 import './App.css'
 
 export class App extends Component {
+  state = {
+    places: []
+  }
+
+  getPlaces = () => {
+    LocationAPI.getLocation("Düsseldorf", "movie theater")
+    .then(data => {
+      this.setState({places: data.response.groups[0].items})
+      console.log(this.state.places)
+    })
+  }
+
   render() {
     const style = {
       width: '100%',
       height: '100%'
     }
-
     return (
       <Map
         google={this.props.google}
+        onReady={this.getPlaces}
         style={style}
         zoom={13}
         initialCenter={{
---
+++ src/LocationAPI.js
@@ -0,0 +1,19 @@
+const api = 'https://api.foursquare.com/v2/venues/explore'
+
+const credentials = {
+  clientId: process.env.REACT_APP_FOURSQUARE_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET,
+}
+
+export const getLocation = (town, query) =>
+  fetch(`${api}?client_id=${credentials.clientId}&client_secret=${credentials.clientSecret}&v=20180323&limit=5&near=${town}&query=${query}`)
+  .then(response => {
+    if (response.status !== 200) {
+      alert('Looks like something went wrong. Status Code: ' + response.status)
+      return
+    }
+    return response.json()
+  })
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err)
+  });
